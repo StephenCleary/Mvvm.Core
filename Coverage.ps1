3aa09f51ec8fac235be8938aeccacb2453785f30
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-15 21:56:05 -0400

    Update to debug version of Coverage.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,4 +1,4 @@
 $artifactLocation = 'artifacts\bin\Nito.Mvvm.Core\Debug\net45'
 $testProjectLocation = 'test/UnitTests'
 $outputLocation = 'testResults'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/6e9b87eabc076c4cd68840d74a10bf9a9579cefb/Coverage.ps1'))
+# iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/9052f57a54d89656df7f63571f525d0f9a22a2ff/Coverage.ps1'))
