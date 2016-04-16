e26b5ac3c0c956d566d040cb78148001f45e46b7
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-15 20:51:22 -0400

    Using new coverage script.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,2 +1,4 @@
 $artifactLocation = 'artifacts\bin\Nito.Mvvm.Core\Debug\net45'
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/master/Coverage.ps1'))
+$testProjectLocation = 'test/UnitTests'
+$outputLocation = 'testResults'
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/6e9b87eabc076c4cd68840d74a10bf9a9579cefb/Coverage.ps1'))
