59efaf2a09a34ba3ee574b52f244385d80add684
Stephen Cleary <github.ourteddybear@xoxy.net>
2016-04-15 22:09:35 -0400

    Uncommented script execution.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,4 +1,4 @@
 $artifactLocation = 'artifacts\bin\Nito.Mvvm.Core\Debug\net45'
 $testProjectLocation = 'test/UnitTests'
 $outputLocation = 'testResults'
-# iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/9052f57a54d89656df7f63571f525d0f9a22a2ff/Coverage.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/9052f57a54d89656df7f63571f525d0f9a22a2ff/Coverage.ps1'))
